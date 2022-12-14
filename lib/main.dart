11f97828dc82e7d4c7da92dfe8fb18e6eaa3e765
Kurogoma939 <kan.sss.939@gmail.com>
2022-12-14 21:33:12 +0900

    Firebase設定・デプロイ設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'application.dart';
+import 'firebase_options.dart';
 import 'initialize.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // Firebase初期化
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   // アプリ実行前の初期化処理
   await Initialize().init();
   // アプリ実行
