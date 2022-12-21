f3fb9fc7f4f29096c2ec53404779bc10e49f84c8
Kurogoma939 <kan.sss.939@gmail.com>
2022-12-21 10:50:42 +0900

    FirestoreのRepository層実装

--- lib/infrastructure/data_source/firestore_data_source.dart
+++ lib/infrastructure/data_source/firestore_data_source.dart
@@ -5,14 +5,33 @@ class FireStoreDataSource {
   /// Firestoreのインスタンス
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  /// Firestoreのコレクションを取得する
-  CollectionReference fetchCollection(String path) {
-    return _firestore.collection(path);
+  /// FirestoreのコレクションをFutureで取得する
+  Future<List<Map<String, dynamic>>> fetchCollectionAsFuture(
+    String path,
+  ) async {
+    final QuerySnapshot snapshot = await _firestore.collection(path).get();
+    final List<Map<String, dynamic>> list = snapshot.docs
+        .map((DocumentSnapshot document) =>
+            document.data() as Map<String, dynamic>)
+        .toList();
+    return list;
+  }
+
+  /// FirestoreのコレクションをStreamで取得する
+  Stream<List<Map<String, dynamic>>> fetchCollectionAsStream(
+    String path,
+  ) {
+    return _firestore.collection(path).snapshots().map(
+        (QuerySnapshot snapshot) => snapshot.docs
+            .map((DocumentSnapshot document) =>
+                document.data() as Map<String, dynamic>)
+            .toList());
   }
 
   /// Firestoreのドキュメントを取得する
-  DocumentReference fetchDocument(String path) {
-    return _firestore.doc(path);
+  Future<Map<String, dynamic>> fetchDocumentAsFuture(String path) async {
+    final DocumentSnapshot snapshot = await _firestore.doc(path).get();
+    return snapshot.data() as Map<String, dynamic>;
   }
 
   /// Firestoreのドキュメントを追加する
--- lib/infrastructure/repository/firesotre_repository.dart
+++ lib/infrastructure/repository/firesotre_repository.dart
@@ -16,4 +16,58 @@ class FireStoreRepository {
   });
 
   final FireStoreDataSource firestoreDataSource;
+
+  /// コレクションをStreamで取得
+  Stream<List<Map<String, dynamic>>> fetchCollectionAsStream(
+    String path,
+  ) {
+    return firestoreDataSource.fetchCollectionAsStream(path);
+  }
+
+  /// コレクションをFutureで取得
+  Future<List<Map<String, dynamic>>> fetchCollectionAsFuture(
+    String path,
+  ) {
+    return firestoreDataSource.fetchCollectionAsFuture(path);
+  }
+
+  /// Firestoreのドキュメントを取得する
+  Future<Map<String, dynamic>> fetchDocumentAsFuture(String path) async {
+    return firestoreDataSource.fetchDocumentAsFuture(path);
+  }
+
+  /// Firestoreのドキュメントを追加する
+  Future<bool> addDocument(String path, Map<String, dynamic> data) async {
+    try {
+      await firestoreDataSource.addDocument(path, data);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  /// Firestoreのドキュメントを更新する
+  Future<bool> updateDocument(String path, Map<String, dynamic> data) async {
+    try {
+      await firestoreDataSource.updateDocument(path, data);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  /// Firestoreのドキュメントを削除する
+  Future<bool> deleteDocument(String path) async {
+    try {
+      await firestoreDataSource.deleteDocument(path);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  /// ドキュメントが存在するかどうかを取得する
+  Future<bool> isExistDocument(String path) async {
+    return firestoreDataSource.isExistDocument(path);
+  }
 }
